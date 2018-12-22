50e09a2523c4bbc05c39c7a84136bf1c543af3dd
peyagodson <peyagodson@ymail.com>
2018-12-22 18:18:17 +0300

    bean lifeCycle

--- src/ru/godsonpeya/spring/TrackCoach.java
+++ src/ru/godsonpeya/spring/TrackCoach.java
@@ -18,5 +18,13 @@ public class TrackCoach implements Coach {
 	public String getDailyFortune() {
 		return "Just Do It "+ fortuneService.getFortune();
 	}
+	
+	public void doMyStartupStuff() {
+		System.out.println("TrackCoach: insite the method doMyStartupStuff");
+	}
+	
+	public void doMyCleanupStuffYoYo() {
+		System.out.println("TrackCoach: insite the method doMyCleanupStuffYoYo");
+	}
 
 }
